2e960dac5f44865534b21ae537328abbe96eeced
BorhanBappy <bappyzx1@gmail.com>
2023-07-15 20:21:43 +0600

    Personal Information

---
+++ main.js
@@ -0,0 +1,11 @@
+var firstname="Borhan";
+console.log(firstname)  ;
+var age='21';
+var isMale=true;
+var ballance= 100.32;
+var dob=new Date(2023,6,5)         
+console.log(age);
+console.log(isMale);
+console.log(ballance);
+console.log(dob);                                                   
+
